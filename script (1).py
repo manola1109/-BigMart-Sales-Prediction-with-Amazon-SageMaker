a0c1cf52e86d7f3462f2d3fd0fb20b5577d96ace
Manola1109 <manola1109@gmail.com>
2025-06-23 00:42:42 +0530

    Add model, dataset, and notebook files

---
+++ script (1).py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1b355c86-caea-484f-8b84-58c879d8582c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
